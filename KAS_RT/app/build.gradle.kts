751aa1a7a485149ee45b719434b457f4b1db07ac
dipca0895 <dipcaanugrah26@gmail.com>
2024-05-07 22:03:14 +0700

    fitur: update notifications

--- KAS_RT/app/build.gradle.kts
+++ KAS_RT/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,6 +46,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-config-ktx:22.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -60,4 +62,10 @@ dependencies {
 
     implementation("com.squareup.okhttp3:logging-interceptor:3.8.0")
     debugImplementation("com.github.chuckerteam.chucker:library:3.3.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    // firebase cloud messaging
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
+
 }
